38f883f4e3bd54a239103e2dc461371026f7e5b5
MarzenkaS <>
2023-09-11 17:43:03 +0000

    Add 'your choice' image references

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,41 @@ document.addEventListener("DOMContentLoaded", function () {
         rockBtnRef.addEventListener('click', ("rock")); {
             console.log('Rock button clicked');
             yourChoice(rock);
-        };
+        }
         paperBtnRef.addEventListener('click', ("paper")); {
             console.log('Paper button clicked');
             yourChoice(paper);
-        };
+        }
         scissorsBtnRef.addEventListener('click', ("scissors")); {
             console.log('Scissors button clicked');
             yourChoice(scissors);
-        };
+        }
 
         // Button references
         const rockBtnRef = document.getElementById('rock');
         const paperBtnRef = document.getElementById('paper');
         const scissorsBtnRef = document.getElementById('scissors');
+
+        //Your choice image references
+        let yourChoiceImageRef = document.getElementById('your-choice');
+
+        function yourChoice(choice) {
+            let yourChoiceImage = document.createElement("img");
+            switch (choice) {
+                case rock:
+                    yourChoiceImage.src = "assets/images/rock.jpg";
+                    break;
+                case scissors:
+                    yourChoiceImage.src = "assets/images/scissors.jpg";
+                    break;
+                case paper:
+                    yourChoiceImage.src = "assets/images/paper.jpg";
+                    break;
+                default:
+                    console.log(choice);
+            }
+            yourChoiceImageRef.appendChild(yourChoiceImage);
+
+        };
 
         
